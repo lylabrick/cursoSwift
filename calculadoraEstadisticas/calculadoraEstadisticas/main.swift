f2207bac1abb9b27582d79a78fa8fedaf49cb1c4
lylabrick <lauradev61@gmail.com>
2026-05-05 14:26:31 -0300

    Limpieza: Remover xcuserdata y aplicar .gitignore

--- calculadoraEstadisticas/calculadoraEstadisticas/main.swift
+++ calculadoraEstadisticas/calculadoraEstadisticas/main.swift
@@ -7,5 +7,24 @@
 
 import Foundation
 
-print("Hello, World!")
+print("--- Calculadora de Estadísticas ---")
+print("Ingresa los números separados por espacios (ej: 10 20 30.5):")
+
+//Leemos la Línea de la consola
+
+if let entrada = readLine(){
+    // Convertimos el string en un array de números Double
+    let numeros = entrada.split(separator: " ").compactMap(Double($0))
+    
+    if numeros.isEmpty {
+        print("No ingresaste números válidos")
+    } else {
+        let suma = numeros.reduce(0, +)
+        let promedio = 
+        
+    }
+    
+    
+}
+
 
